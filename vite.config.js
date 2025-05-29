23837f232cc07e23d3b71499275da93f763a0c47
akinblaq <ogundelekn@gmail.com>
2025-05-29 20:20:27 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": "http://localhost:5000",
     },
   },
-
-  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  // 👇 SPA fallback for React Router (very important for Vercel)
+  base: "/",
 });
